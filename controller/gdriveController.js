ca86cec817f7110ffe995c23d4b71b4187fbb37a
Parag Jadhav <parag242001@gmail.com>
2021-05-18 10:13:31 +0530

    test function to fetch google drive files

--- controller/gdriveController.js
+++ controller/gdriveController.js
@@ -1,9 +1,21 @@
-const googleDriveAuth = require('../config/googleDriveAuth');
+// const googleDriveAuth = require('../config/googleDriveAuth');
+const { google } = require('googleapis');
 
-module.exports.test = async () => {
+module.exports.test = async (query) => {
   try {
-    console.log('test', googleDriveAuth);
+    console.log('GDRIVE_API_KEY', process.env.GDRIVE_API_KEY);
+    const drive = google.drive({
+      version: 'v3',
+      auth: process.env.GDRIVE_API_KEY,
+    });
+
+    const params = { pageSize: 3 };
+    params.q = query;
+    const res = await drive.files.list(params);
+    console.log(res.data);
+    return res.data;
   } catch (error) {
     console.log('Error', error);
+    throw error;
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const gdriveController = require('../controller/gdriveController');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.send(`Server is running on port ${process.env.HEROKU_SERVER_PORT}`);
+  res.send(`Server is running on port ${process.env.PORT}`);
 });
 
-router.get('/test', gdriveController.test);
+router.get('/test', (req, res) => {
+  const data = gdriveController.test(req.query.q);
+  res.status(200).send(data);
+});
 
 module.exports = router;
